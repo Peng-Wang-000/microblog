fdddde3d7652718cc59853de4603e552ca98f16c
wangpengpeng <wangpengpeng@nibirutech.com>
2018-08-28 10:32:03 +0800

    数据库操作 1、使用数据库sqlite 2、orm框架Flask-SQLAlchemy 3、数据库版本控制SQLAlchemy-migrate 4、脚本文件db_*.py 5、数据库models(User,Post) 6、在config.py中添加数据库配置

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 # 创建了flask实例对象
 app = Flask(__name__)
 # 加载配置文件
 app.config.from_object('config')
+db = SQLAlchemy(app)
 
-from app import views
+from app import views, models
